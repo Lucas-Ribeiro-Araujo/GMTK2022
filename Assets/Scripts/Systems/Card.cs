573ba62020ab81bdf924e58bebf7011ac7a553d6
Lucas Araújo <araujolucas0427@gmail.com>
2022-07-16 15:51:03 -0300

    HP and Unit updates

---
+++ Assets/Scripts/Systems/Card.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+  public void OnTakeDamage(object sender, EventArgs e)
+    {
+        Debug.Log("i die: " + gameObject.name);
+        Destroy(gameObject);
+    }
+
+}
--- Assets/Scripts/Systems/HPManager.cs
+++ Assets/Scripts/Systems/HPManager.cs
@@ -1,18 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HPManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static HPManager Instance;
+
+    [SerializeField]
+    List<Card> Cards = new List<Card>();
+
+    public event EventHandler damageEvent;
+
+    public event EventHandler deathEvent;
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            TakeDamage();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        Instance = this;
+    }
+
+    public void TakeDamage()
+    {
+        if(Cards.Count > 0)
+        {
+        damageEvent += Cards[Cards.Count - 1].OnTakeDamage;
+        damageEvent?.Invoke(this, EventArgs.Empty);
+        damageEvent -= Cards[Cards.Count - 1].OnTakeDamage;
+        Cards.Remove(Cards[Cards.Count - 1]);
+        }
+        else
+        {
+            deathEvent?.Invoke(this, EventArgs.Empty);
+            Debug.Log("I is die!");
+        }
     }
 }
