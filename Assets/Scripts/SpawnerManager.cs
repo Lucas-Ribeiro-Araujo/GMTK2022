df01144df1152042eceaed9cb506504ab47bb8ef
victorlahiguera <victorlahiguera@gmail.com>
2022-07-16 02:07:38 -0300

    Spawner Manager WIP

--- Assets/Scripts/SpawnerManager.cs
+++ Assets/Scripts/SpawnerManager.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 public class SpawnerManager
 {
     public GameObject[] spawner;
+    int maxSpawnerQuantity;
+    public bool hasSpawned;
     
 
     public void Start()
     {
-        
-      
+        maxSpawnerQuantity = spawner.Length;
+        hasSpawned = false;
+    }
+
+    private void SpawnEnemyOnSelectedSpawner()
+    {
+        if (spawner.Length != 0)
+        {
+            int spawnerRange = Random.Range(0, maxSpawnerQuantity);
+            GameManager.Instance.timeTick.OnTick += spawner[spawnerRange].GetComponent<EnemySpawner>().SpawnEnemy;
+            hasSpawned = true;
+        }
+
     }
 
     // Update is called once per frame
     public void Update()
     {
+        if (!hasSpawned)
+        {
+            SpawnEnemyOnSelectedSpawner();
+        }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
